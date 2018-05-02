3c2151d664c7a76c4fe73df368bdb49d5b3277d3
HampusS <supmahampus@gmail.com>
2018-05-02 16:00:19 +0200

    Added products routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+$router->get('/Products', 'ProductsController@index');
+$router->get('/Products/{id}', 'ProductsController@show');
